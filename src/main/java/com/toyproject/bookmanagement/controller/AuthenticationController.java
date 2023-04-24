3b77c2b936c134ad1426d9a56e2eb458524a6c42
dlgustn3768 <dlgustn3768@naver.com>
2023-04-24 14:59:54 +0900

    Principal init

--- src/main/java/com/toyproject/bookmanagement/controller/AuthenticationController.java
+++ src/main/java/com/toyproject/bookmanagement/controller/AuthenticationController.java
@@ -47,4 +47,9 @@ public class AuthenticationController {
 		return ResponseEntity.ok().body(authenticationService.authenticated(accessToken));
 	}
 	
+	@GetMapping("/principal")
+	public ResponseEntity<?> principal(String accessToken){
+		return ResponseEntity.ok().body(authenticationService.getPrincipal(accessToken));
+	}
+	
 }
